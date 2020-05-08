23f34c6fec34dc8aeca030c06ade778244f9b41d
Fahim G <farg1@student.le.ac.uk>
2020-05-09 00:12:57 +0100

    upgrade to terraform 0.12

--- find_ubuntu_ami.tf
+++ find_ubuntu_ami.tf
@@ -1,19 +1,17 @@
-data "aws_ami" "ubuntu"
-{
+data "aws_ami" "ubuntu" {
   most_recent = "true"
- owners = ["099720109477"]
-  filter
-  {
-  name = "name"
-  values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
   }
- filter
- {
-  name = "virtualization-type"
-  values = ["hvm"]
- }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
-output "aws_ami" "ubunut"
-{
-  value = "${data.aws_ami.ubuntu.id}"
+
+output "aws_ami" {
+  value = data.aws_ami.ubuntu.id
 }
+
